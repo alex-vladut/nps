5f44245c5f3eebc191a25ebd05aae5c18dec95d5
Alex Vladut <alexandruvladut@gmail.com>
2021-03-08 16:47:09 +0200

    chore: initial project structure

---
+++ apps/nps-api/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'nps-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/nps-api',
+};
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  projects: ['<rootDir>/apps/nps-web', '<rootDir>/apps/nps-api'],
+};
